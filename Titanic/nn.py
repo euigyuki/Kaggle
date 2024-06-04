82b537e4592866c66602eaf49efba263e2454b4f
Derrick <egk265@github.com>
2024-06-03 23:11:40 -0400

    updated kaggle dataset code

--- Titanic/nn.py
+++ Titanic/nn.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
-#why the fuck is this working now
 
 # Load the dataset
 data = pd.read_csv('train.csv')
